9f127ef056bfd3df25d1d918575e837da770af63
akrm al-hakimi <alhakimiakrmj@gmail.com>
2024-11-25 16:48:55 -0500

    modularized code

---
+++ src/models/week.py
@@ -0,0 +1,15 @@
+# src/models/week.py
+from typing import List
+from .assignment import Assignment
+
+class Week:
+    def __init__(self, week_number: int):
+        self.week_number = week_number
+        self.assignments: List[List[Assignment]] = [[] for _ in range(7)]
+
+    def add_assignment(self, day: int, name: str):
+        self.assignments[day].append(Assignment(name))
+
+    def toggle_assignment_completion(self, day: int, assignment_index: int):
+        if 0 <= day < 7 and 0 <= assignment_index < len(self.assignments[day]):
+            self.assignments[day][assignment_index].toggle_completion()
---
+++ src/utils/constants.py
@@ -0,0 +1,34 @@
+# src/utils/constants.py
+CHECK_MARK = "*"
+CROSS_MARK = " "
+BULLET = ">"
+CURSOR = "_"
+ACTIVE_DOT = "●"
+
+# Menu state enum
+from enum import Enum
+
+class MenuState(Enum):
+    MAIN = 1
+    SEMESTER = 2
+    WEEK = 3
+    DAY = 4
+    ASSIGNMENTS = 5
+
+# Startup and shutdown messages
+STARTUP_MESSAGES = [
+    "ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM v1.0",
+    "COPYRIGHT 2024-2025 ROBCO INDUSTRIES",
+    "LOADER V1.1",
+    "EXEC VERSION 41.10",
+    "-SERVER 1-"
+]
+
+SHUTDOWN_MESSAGES = [
+    "SHUTTING DOWN ROBCO TERMINAL",
+    "SAVING SESSION DATA...",
+    "CLEARING MEMORY...",
+    "GOODBYE."
+]
+
+DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
